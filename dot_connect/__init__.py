2fee4fb702c4216ae5e2e21b10706b8338952e14
Tyler White <tyler.white@snowflake.com>
2023-08-15 08:59:13 -0400

    feat: add postgres

--- dot_connect/__init__.py
+++ dot_connect/__init__.py
@@ -1,1 +1,7 @@
 """The module for making connections easier."""
+from dot_connect.backends import postgres, snowflake, snowpark
+
+
+def list_backends() -> list:
+    """List compatible dot-connect backends."""
+    return ["postgres", "snowflake", "snowpark"]
