4e53b5dcf96aae1229c21f3bd3eaf387bec76ada
Tyler White <50381805+IndexSeek@users.noreply.github.com>
2023-08-21 09:06:06 -0400

    feat: add cloud provider support

--- dot_connect/__init__.py
+++ dot_connect/__init__.py
@@ -1,35 +1,49 @@
 """The module for making connections easier."""
+
+import contextlib
+
 from dot_connect.backends import load_config
 
-try:
+with contextlib.suppress(ImportError):
+    from dot_connect.backends import aws
+with contextlib.suppress(ImportError):
+    from dot_connect.backends import azure
+with contextlib.suppress(Exception):
+    from dot_connect.backends import impala
+with contextlib.suppress(Exception):
     from dot_connect.backends import mssql
-except:  # noqa E722
-    pass
-
-try:
+with contextlib.suppress(ImportError):
     from dot_connect.backends import mysql
-except ImportError:
-    pass
-
-try:
+with contextlib.suppress(ImportError):
     from dot_connect.backends import postgres
-except ImportError:
-    pass
-
-try:
+with contextlib.suppress(ImportError):
     from dot_connect.backends import snowflake
-except ImportError:
-    pass
-
-try:
+with contextlib.suppress(ImportError):
     from dot_connect.backends import snowpark
-except ImportError:
-    pass
 
 
 def list_backends() -> list:
     """List compatible dot-connect backends."""
-    return ["mssql", "mysql", "postgres", "snowflake", "snowpark"]
+    return [
+        "aws",
+        "azure",
+        "impala",
+        "mssql",
+        "mysql",
+        "postgres",
+        "snowflake",
+        "snowpark",
+    ]
 
 
-__all__ = ["load_config", "mssql", "mysql", "postgres", "snowflake", "snowpark"]
+__all__ = [
+    "load_config",
+    "aws",
+    "azure",
+    "impala",
+    "mssql",
+    "mysql",
+    "postgres",
+    "snowflake",
+    "snowpark",
+]
