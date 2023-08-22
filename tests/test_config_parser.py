1b4173da2d47ae6a6d17826b6322bb99b88e2b5d
Tyler White <90005851+sfc-gh-twhite@users.noreply.github.com>
2023-08-22 20:45:28 +0000

    v0.3.3 (#21)
    * chore: add req install options
    
    * Create python-package-conda.yml
    
    * chore: remove conda snowpark pin
    
    * chore: more snowpark to conda dep
    
    * ci: install conda env
    
    * Create dependency-review.yml
    
    * Create stale.yml
    
    * [pre-commit.ci] pre-commit autoupdate
    
    updates:
    - [github.com/pre-commit/pre-commit-hooks: v3.2.0 → v4.4.0](https://github.com/pre-commit/pre-commit-hooks/compare/v3.2.0...v4.4.0)
    - [github.com/pre-commit/mirrors-mypy: v1.5.0 → v1.5.1](https://github.com/pre-commit/mirrors-mypy/compare/v1.5.0...v1.5.1)
    
    * docs: Update README.md
    
    * docs/create-templates
    
    * docs: create CODE_OF_CONDUCT.md
    
    * Add file parsing and linting support for new packages (#8)
    
    * Add file parsing and linting support for new packages
    
    * chore: linting ruff
    
    * Fix ini import in parse_files
    
    * Create test_config_parser.py
    
    * feat: add tests for config parsing
    
    * chore: lint
    
    * chore: fix pre-commit
    
    ---------
    
    Co-authored-by: sfc-gh-jsummer <jason.summer@snowflake.com>
    Co-authored-by: Tyler White <50381805+IndexSeek@users.noreply.github.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * 20 feat utility to convert snowflake credentials to sqlalchemy compatible url (#22)
    
    * Update __init__.py
    
    * Update snowflake.py
    
    * add back config loads
    
    * feat(snowflake): add url formatter
    
    * docs: add docstrings for format_url
    
    * docs: update version
    
    ---------
    
    Co-authored-by: Tyler White <50381805+IndexSeek@users.noreply.github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: sfc-gh-jsummer <jason.summer@snowflake.com>

---
+++ tests/test_config_parser.py
@@ -0,0 +1,71 @@
+"""Tests for the various utilities in the dot_connect package."""
+
+from dot_connect.backends import load_config, parse_file
+
+# Sample content for JSON, YAML, and INI files
+json_content = """
+{
+    "account": "abc.us-east-1",
+    "user": "JSMITH",
+    "password": "password123"
+}
+"""
+
+yaml_content = """
+account: "abc.us-east-1"
+user: "JSMITH"
+password: "password123"
+"""
+
+ini_content = """
+[credentials]
+account = abc.us-east-1
+user = JSMITH
+password = password123
+"""
+
+
+def test_read_json(tmp_path):
+    """Test reading of JSON content from a sample JSON file using the `parse_file` function."""
+    file = tmp_path / "config.json"
+    file.write_text(json_content)
+    result = parse_file(str(file))
+    assert result == {
+        "account": "abc.us-east-1",
+        "user": "JSMITH",
+        "password": "password123",
+    }
+
+
+def test_read_yaml(tmp_path):
+    """Test reading of YAML content from a sample YAML file using the `parse_file` function."""
+    file = tmp_path / "config.yaml"
+    file.write_text(yaml_content)
+    result = parse_file(str(file))
+    assert result == {
+        "account": "abc.us-east-1",
+        "user": "JSMITH",
+        "password": "password123",
+    }
+
+
+def test_read_ini(tmp_path):
+    """Test reading of INI content from a sample INI file using the `parse_file` function."""
+    file = tmp_path / "config.ini"
+    file.write_text(ini_content)
+    result = parse_file(str(file))
+    assert result == {
+        "credentials": {
+            "account": "abc.us-east-1",
+            "user": "JSMITH",
+            "password": "password123",
+        }
+    }
+
+
+def test_load_config_from_env(monkeypatch):
+    """Test extraction of config from environment variables using `load_config`."""
+    monkeypatch.setenv("SNOWFLAKE_USER", "admin")
+    monkeypatch.setenv("SNOWFLAKE_PASSWORD", "secret")
+    result = load_config("SNOWFLAKE")
+    assert result == {"user": "admin", "password": "secret"}
