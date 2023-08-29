3fa6c6f74c8b0b7996c5cf24fe7fe39fe4c5fb5c
Tyler White <50381805+IndexSeek@users.noreply.github.com>
2023-08-28 22:11:08 -0400

    tests: refactor higher cov

--- tests/test_config_parser.py
+++ tests/test_config_parser.py
@@ -1,63 +1,79 @@
-"""Tests for the various utilities in the dot_connect package."""
+"""This module provides utilities for parsing backend configuration."""
 
-from dot_connect.backends import parse_file
+import json
 
-# Sample content for JSON, YAML, and INI files
-json_content = """
-{
-    "account": "abc.us-east-1",
-    "user": "JSMITH",
-    "password": "password123"
-}
-"""
+import yaml
 
-yaml_content = """
-account: "abc.us-east-1"
-user: "JSMITH"
-password: "password123"
-"""
-
-ini_content = """
-[credentials]
-account = abc.us-east-1
-user = JSMITH
-password = password123
-"""
+from dot_connect import list_backends, load_config, parse_file, read_json, read_yaml
 
 
 def test_read_json(tmp_path):
-    """Test reading of JSON content from a sample JSON file using the `parse_file` function."""
-    file = tmp_path / "config.json"
-    file.write_text(json_content)
-    result = parse_file(str(file))
-    assert result == {
-        "account": "abc.us-east-1",
-        "user": "JSMITH",
-        "password": "password123",
-    }
+    """Test the read_json function with valid JSON data."""
+    data = {"key": "value"}
+    file_path = tmp_path / "data.json"
+    with open(file_path, "w") as f:
+        json.dump(data, f)
+
+    result = read_json(file_path)
+    assert result == data
 
 
 def test_read_yaml(tmp_path):
-    """Test reading of YAML content from a sample YAML file using the `parse_file` function."""
-    file = tmp_path / "config.yaml"
-    file.write_text(yaml_content)
-    result = parse_file(str(file))
-    assert result == {
-        "account": "abc.us-east-1",
-        "user": "JSMITH",
-        "password": "password123",
-    }
+    """Test the read_yaml function with valid YAML data."""
+    data = {"key": "value"}
+    file_path = tmp_path / "data.yaml"
+    with open(file_path, "w") as f:
+        yaml.dump(data, f)
+
+    result = read_yaml(file_path)
+    assert result == data
 
 
-def test_read_ini(tmp_path):
-    """Test reading of INI content from a sample INI file using the `parse_file` function."""
-    file = tmp_path / "config.ini"
-    file.write_text(ini_content)
-    result = parse_file(str(file))
-    assert result == {
-        "credentials": {
-            "account": "abc.us-east-1",
-            "user": "JSMITH",
-            "password": "password123",
-        }
-    }
+def test_parse_file_json(tmp_path):
+    """Test the parse_file function for JSON file type."""
+    data = {"key": "value"}
+    file_path = tmp_path / "data.json"
+    with open(file_path, "w") as f:
+        json.dump(data, f)
+
+    result = parse_file(str(file_path))
+    assert result == data
+
+
+def test_parse_file_unsupported_type(tmp_path):
+    """Test the parse_file function for unsupported file types."""
+    file_path = tmp_path / "data.txt"
+    file_path.write_text("just some text")
+
+    result = parse_file(str(file_path))
+    assert (
+        result
+        == "File must be of type ('.json', '.yaml', '.yml', '.ini', '.cfg', '.conf')."  # noqa: W503
+    )
+
+
+def test_load_config_from_file_json(tmp_path):
+    """Test the load_config function with a JSON configuration file."""
+    data = {"key": "value"}
+    file_path = tmp_path / "data.json"
+    with open(file_path, "w") as f:
+        json.dump(data, f)
+
+    result = load_config("", str(file_path))
+    assert result == data
+
+
+def test_list_backends():
+    """Test the list_backends function to ensure all backends are listed correctly."""
+    backends = list_backends()
+    expected_backends = [
+        "aws",
+        "azure",
+        "mssql",
+        "mysql",
+        "postgres",
+        "pyspark",
+        "snowflake",
+        "snowpark",
+    ]
+    assert backends == expected_backends
